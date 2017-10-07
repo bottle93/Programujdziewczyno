59f73e39e824225a3618927c71aac5e83fef304b
root <alestrzelec@gmail.com>
2017-10-07 15:45:41 +0200

    zadanie1

---
+++ JS/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var forDropdown = document.querySelector('.for-dropdown');
+    var contents = document.querySelector(".dropdown");
+    forDropdown.addEventListener('mouseover', function(){
+        contents.style.display = "block";
+    })
+    forDropdown.addEventListener("mouseout", function(){
+        contents.style.display = "none";
+    })
+});
